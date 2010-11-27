daad08db3a5b1b42781072561ef2452facdafb4e
Jaakko Manninen <kschzt@gmail.com>
2010-11-27 03:32:27 +0200

    fix preinstall script for mac

--- scripts/preinstall.sh
+++ scripts/preinstall.sh
@@ -1,1 +1,19 @@
-if [ "$(locate linux-tick-processor.py)" == "" ]; then echo "Couldn't find linux-tick-processor.py"; exit 1; fi
+PLATFORM=`uname`
+if [ $PLATFORM == "Darwin" ]
+then
+    if [ "$(locate mac-tick-processor)" == "" ]
+        then 
+        echo "Couldn't find linux-tick-processor.py"
+        exit 1
+    fi
+elif [ $PLATFORM == "Linux" ]
+then
+    if [ "$(locate linux-tick-processor.py)" == "" ]
+    then 
+        echo "Couldn't find linux-tick-processor.py"
+        exit 1
+    fi
+else
+    echo "Unknown platform"
+fi
+
